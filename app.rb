3f9a6bc8278a1d6b0abcf51fca683e87b4d088cc
Mike Merin <mikemerinweather@gmail.com>
2017-04-26 16:18:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,14 +3,15 @@ require_relative 'models/textanalyzer.rb'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-post '/' do
-  text_from_user = params[:user_text]
-  erb :results
-end
-
+  post '/' do
+    @text = TextAnalyzer.new(params[:user_text])
+    @most_letter = @text.most_used_letter[0]
+    @most_count = @text.most_used_letter[1]
+    erb :results
+  end
 
 end
--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -1,1 +1,27 @@
-#your text analyzer model code will go here.
+class TextAnalyzer
+
+  attr_accessor :text
+
+  def initialize(text) @text = text end
+
+  def count_of_words
+    @text.split(" ").count
+  end
+
+  def count_of_vowels
+    @text.downcase.scan(/[aeoui]/).count
+  end
+
+  def count_of_consonants
+    @text.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+
+    count = Hash.new(0)
+    @text.downcase.scan(/[a-z]/) { |x| count[x] += 1 }
+    count.max_by { |k,v| v }
+
+  end
+
+end
